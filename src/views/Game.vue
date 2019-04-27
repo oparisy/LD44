e96bc3720501f3769cff706663fee6f8d971ad3d
oparisy <olivier.parisy@gmail.com>
2019-04-27 13:18:00 +0200

    Isometric camera sample

---
+++ src/views/Game.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is the game page</h1>
+    <MainCanvas />
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import MainCanvas from '@/components/MainCanvas.vue'
+
+export default {
+  name: 'game',
+  components: {
+    MainCanvas
+  }
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   // The github project name (required for proper deployment of dist/)
-  publicPath: 'LD44'
+  publicPath: 'LD44',
+
+  // Auto fix linting errors
+  // From https://www.reddit.com/r/vuejs/comments/8kn8dh/autofix_linting_errors_with_vuecli_3/e2rz9bv?utm_source=share&utm_medium=web2x
+  chainWebpack: config => {
+    config.module.rule('eslint').use('eslint-loader').options({
+      fix: true
+    })
+  }
 }
