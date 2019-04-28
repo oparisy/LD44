e5e89c1db2e15dc1c9b8f7c071e6db266d496dd6
oparisy <olivier.parisy@gmail.com>
2019-04-28 22:48:25 +0200

    Control panel CSS cleanup

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="controlroot">
-    <div id="level"><span class="label">Level: </span><span class="value">{{gameController.level}}</span></div>
+    <div id="level">
+      <span class="label">Level {{gameController.level}}</span>
+      <span class="value"> - </span>
+      <span class="hour">{{Math.floor(gameController.hour)}}h00</span></div>
     <div id="currency"><span class="label">Currency: </span><span class="value">{{gameController.credits}}</span></div>
-    <div id="clock"><span class="label">Clock: </span><span class="value">{{Math.floor(gameController.hour)}}h00</span></div>
     <div id="actions">
       <span class="label">Planted on click:</span><br/>
+      <div id="radios">
       <input type="radio" id="oak" value="oak" v-model="gameController.toPlant"><label for="oak">Oak</label><br/>
       <input type="radio" id="pine" value="pine" v-model="gameController.toPlant"><label for="pine">Pine</label><br/>
       <input type="radio" id="poplar" value="poplar" v-model="gameController.toPlant"><label for="poplar">Poplar</label><br/>
+      </div>
+      <div id="hint">(Click on a tree to cut it)</div>
     </div>
     <div id="nextlevel"><button type="button" v-on:click="onNextLevel">Buy Bigger Land</button></div>
     <div id="navbox">
+      <div id="nav">
       <router-link to="/">Back to Home</router-link>
+      </div>
     </div>
 
   </div>
@@ -42,20 +49,54 @@ export default {
     background-color: lightslategrey;
 
     border-radius: 10px;
+
+    font-family: 'Zilla Slab', serif;
+    font-size: 150%;
   }
 
-  #level {
-    margin-top: 15px;
+  .label {
+    font-size: 150%;
+  }
+
+  .hour {
+    display: inline-block;
+    width: 2.5em;
   }
 
   #actions {
     margin-top: 30px;
     flex: 1 1 auto;
   }
+
+  #radios {
+    display: flex;
+    flex-direction: row;
+    align-self: center;
+    align-items: center;
+    justify-content: center;
+  }
+
+  #hint {
+    font-size: 75%;
+  }
+
   #nextlevel {
     margin-top: 15px;
     flex: 5 5 auto;
   }
+
+  #nextlevel > button {
+    background-color: #2c3e50;
+    color: cadetblue;
+    border: none;
+    border-radius: 5px;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+
   #navbox {
      margin-top:auto;
      margin-bottom:15px;
